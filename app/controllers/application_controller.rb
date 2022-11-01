a272a40622c46543cb58e4cbdf21907140873f1d
myhearisranch <myhearisranch@gmail.com>
2022-11-02 01:39:12 +0900

    新規登録 , ログイン機能 作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    def clock_in?(attendance)
+        return false if attendance.nil?
+        !attendance.clock_in_at.nil?
+    end
+
+    def clock_out?(attendance)
+        return false if attendance.nil?
+        !attendance.clock_out_at.nil?
+    end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_01_145901) do
+ActiveRecord::Schema.define(version: 2022_11_01_162728) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.datetime "clock_in_at"
+    t.datetime "clock_out_at"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
