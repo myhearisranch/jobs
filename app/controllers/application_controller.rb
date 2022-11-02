3e16e4d0e6dab7c04f73cf4a02db348969373df2
myhearisranch <myhearisranch@gmail.com>
2022-11-02 12:43:15 +0900

    ユーザー名前 表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!,except: [:top, :about]
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     def clock_in?(attendance)
         return false if attendance.nil?
@@ -9,4 +11,11 @@ class ApplicationController < ActionController::Base
         return false if attendance.nil?
         !attendance.clock_out_at.nil?
     end
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      end
+
 end
