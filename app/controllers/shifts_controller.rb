e7d470329c2a935c520923acc570428e3aea40f6
root <root@DESKTOP-2O6QUN5.localdomain>
2023-04-13 19:14:28 +0900

    シフト作成機能 追加

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -4,5 +4,25 @@ class ShiftsController < ApplicationController
     @shift = Shift.new
   end
 
+  def create
+    @shift = Shift.new(shift_params)
+    if @shift.save
+     #redirect_to shifts_path,  notice: "スケジュールを作成しました。"
+    else
+      render :new
+    end
+  end
+
+  def index
+    @shifts = Shift.all
+  end
+
+  
+
+  private
+  def shift_params
+    params.require(:shift).permit(:start_time, :end_time, :user_id)
+  end
+
  
 end
